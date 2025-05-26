bbedcb5b72ca54a414c92ff4dbff903e89ec5ed2
Darsu <95997959+dar-su@users.noreply.github.com>
2025-05-26 19:36:31 +0700

    let arc9 know all guns are capable of inspects

--- lua/weapons/arc9_eft_mk18_mjolnir.lua
+++ lua/weapons/arc9_eft_mk18_mjolnir.lua
@@ -451,6 +451,10 @@ SWEP.Animations = {
     },    
     
 
+    ["inspect"] = { -- shutup arc9 we have inspect
+        Source = "idle",
+        Time = 0.05,
+    },
 
     ["inspect1"] = {
         Source = "inspect0",
--- lua/weapons/arc9_eft_vpo101.lua
+++ lua/weapons/arc9_eft_vpo101.lua
@@ -556,6 +556,10 @@ SWEP.Animations = {
     },    
     
 
+    ["inspect"] = { -- shutup arc9 we have inspect
+        Source = "idle",
+        Time = 0.05,
+    },
 
     ["inspect1"] = {
         Source = "look0",
